0e76ecb0562b286c40045e262fbe30195fcbbbb8
sub cruru <crurumain@gmail.com>
2024-11-14 21:29:05 +0900

    debug1

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 let urls = location.pathname.split('/')
 
 let imgurl = urls[urls.length - 1].replace(',', '/')
+
+console.log(imgurl)
+
 let imgEle = document.getElementById("dimg")
 
 imgEle.src = "data:image/png,base64," + imgurl;
