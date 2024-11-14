58a7a2ba7f84b066badc35968b40772f82bc79f3
sub cruru <crurumain@gmail.com>
2024-11-14 21:20:04 +0900

    다시

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 let urls = location.pathname.split('/')
 
 let imgurl = urls[urls.length - 1].replace(',', '/')
-
 let imgEle = document.getElementById("dimg")
 
 imgEle.src = "data:image/png,base64," + imgurl;
