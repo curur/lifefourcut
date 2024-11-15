375e0a997cf6058a8da1dee61ff6b02e5844ab13
curur <crurumain@gmail.com>
2024-11-15 23:43:53 +0900

    ;을 써야 했는데 ,로 씀

--- script.js
+++ script.js
@@ -1,9 +1,6 @@
 let urls = location.pathname.split('/')
 
-let imgurl = urls[urls.length - 1].replace(',', '/')
-
-console.log(imgurl)
-
+let imgurl = urls[urls.length - 1]
 let imgEle = document.getElementById("dimg")
 
-imgEle.src = "data:image/png,base64," + imgurl;
+imgEle.src = "data:image/png;base64," + imgurl.replaceAll(',', '/');
